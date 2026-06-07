9737f989b5995571d73716dae3cd348f01cceda5
jackob from arch linux <jakubcios99@gmail.com>
2026-06-07 19:51:58 +0200

    Add monitor speed, fix board ID, minimal main with logging

--- esp32/src/main.c
+++ esp32/src/main.c
@@ -1,1 +1,15 @@
-void app_main() {}
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+static const char *TAG = "main";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Mini-EOL firmware started");
+
+    while (1) {
+        ESP_LOGI(TAG, "alive");
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
